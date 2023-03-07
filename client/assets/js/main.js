bafa0efa66d4ec9da790c555d38792613213fdd7
NovatXAI <127242078+NovatXAI@users.noreply.github.com>
2023-03-07 22:11:23 +0200

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://novatx-ai-chat-bot.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
